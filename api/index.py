e5fa921818f39381c7d1b3e3593c22486e2feb38
skolhe74 <sohamkolhe@outlook.com>
2025-08-14 03:02:44 -0700

    Fix: Remove non-existent vercel-wsgi, use standard Django WSGI

--- api/index.py
+++ api/index.py
@@ -1,9 +1,8 @@
 import os
-from vercel_wsgi import handle
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
 
 # Vercel expects a handler named `app`
-app = handle(application)
+app = application
